acdd22bd389eaa538ac8d2110fefc1e415d9c7dd
hawktre <trent.vanhawkins@gmail.com>
2025-04-07 12:37:35 -0700

    removed old trevor files

--- Code/Archive/cdm_project/Raw_Zedx_data/2012_data/shortcut.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "This is a folder file manipulation script" 
-
-ls -1F > mylisting.txt
-sed 's/\'_wxdata.csv'//' mylisting.txt > new.txt
-sed 's/_/ /' new.txt > new_listing.txt
-sed 's/_/ /' new_listing.txt > new_listing2.txt
-sed 's/ /,/' new_listing2.txt > new_listing.csv
