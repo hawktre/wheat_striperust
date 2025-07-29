3b4ceaf076207cac3b50c1b94824dd82cbb9546b
hawktre <trent.vanhawkins@gmail.com>
2025-07-29 13:52:06 -0700

    switching simulations to engineering HPC

--- Code/04b_RunSim.R
+++ Code/04b_RunSim.R
@@ -11,27 +11,46 @@
 ##
 ## ---------------------------
 
-## load up the packages we will need:  (uncomment as required)
+# setwd("/scratch/hawkintr/wheat_striperust_sim")
+
+# ## load up the packages we will need:  (uncomment as required)
+# options(repos = c(CRAN = "https://cloud.r-project.org"))
+
+# pkgs <- c("data.table", "dplyr", "purrr", "stringr",
+#           "lubridate", "tidyr", "furrr", "MASS")
+
+# install.packages(pkgs, dependencies = TRUE)
+
 library(here)
 library(data.table)
-library(tidyverse)
-library(furrr)
+library(dplyr)
+library(stringr)
+library(lubridate)
+library(tidyr)
 library(MASS)
+library(purrr)
+library(parallel)  # Use base parallel package
 source(here("Code/02a_GradDescentFun.R"))
 source(here("Code/03a_EMgradfun.R"))
 source(here("Code/04a_SimFunc.R"))
 
-# Read in the data --------------------------------------------------------
+# Read in data
 forward_fits <- readRDS(here("DataProcessed/results/forward_model/forward_fits.rds"))
 mod_dat <- readRDS(here("DataProcessed/experimental/mod_dat_arrays.rds"))
 
-## Set up Simulation
-plan(multicore(workers = availableCores()))
+# Set up parallel simulation
 nsim <- 10000
+ncores <- detectCores(logical = FALSE)  # Physical cores only (optional)
+ncores <- min(nsim, ncores)
 
-## Run the simulation
 set.seed(404)
-sims <- 1:nsim %>% future_map_dfr(~single_sim(.x, mod_dat, forward_fits, output_dir = here("DataProcessed/results/simulation/errors/")))
+sim_list <- mclapply(1:nsim, function(i) {
+  single_sim(i, mod_dat, forward_fits,
+             output_dir = here("DataProcessed/results/simulation/errors/"))
+}, mc.cores = ncores)
 
+# Combine results into a data.frame
+sims <- rbindlist(sim_list)
 
+# Save
 saveRDS(sims, here("DataProcessed/results/simulation/sims.rds"))
