454a18a5b2475ecf10a9e6c96a185dfa104a2926
hawktre <trent.vanhawkins@gmail.com>
2025-07-29 15:17:59 -0700

    edited rscript.sh

--- Code/Rscript.sh
+++ Code/Rscript.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH -J sampleR       # name of my job
+#SBATCH -J StripeSim       # name of my job
 #SBATCH -p share         # name of partition/queue to use
-#SBATCH -o sampleR.out        # name of output file for batch script
-#SBATCH -e sampleR.err        # name of error file for batch script 
-#SBATCH -c 2             # number of cores per task
-#SBATCH --mem=10g        # memory needed for job
+#SBATCH -o stripe_sim.out        # name of output file for batch script
+#SBATCH -e stripe_sim.err        # name of error file for batch script 
+#SBATCH -c 10            # number of cores per task
+#SBATCH --mem=20G        # memory needed for job
 #SBATCH --time=3-00:00:00  # time needed for job
 
 # gather basic information, can be useful for troubleshooting
@@ -13,16 +13,17 @@ echo $SLURM_JOBID
 
 # load modules needed for job
 
-module load R/4.2.2
+module load R
 
 # run my job
 date
 
 # run R code, print output to file "hello.out"
-Rscript hello.R > hello.out 
+Rscript --vanilla Code/04b_RunSim.R
 
-sleep 100
+# print date and time when job is finished
 date
-
+echo "R job completed successfully."
+# end of script
 exit
 
