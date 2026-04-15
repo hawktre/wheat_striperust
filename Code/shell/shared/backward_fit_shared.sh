3626e01a43a2c63eb1e7eed8e144a6bcb8f92910
hawktre <trent.vanhawkins@gmail.com>
2026-04-15 16:07:56 -0700

    updated number of cores in use to 1

--- Code/shell/shared/backward_fit_shared.sh
+++ Code/shell/shared/backward_fit_shared.sh
@@ -2,7 +2,7 @@
 #SBATCH -J BackwardFitShared                # Job name
 #SBATCH -o DataProcessed/results/backward_model/logs/backward_fit_shared_%A_%a.out     # Stdout log (%A=job ID, %a=array index)
 #SBATCH -e DataProcessed/results/backward_model/logs/backward_fit_shared_%A_%a.err     # Stderr log
-#SBATCH -c 10                         # CPU cores per task
+#SBATCH -c 1                        # CPU cores per task
 #SBATCH --mem=4G                     # Memory per task
 #SBATCH --time=01:00:00             # Walltime per task
 
