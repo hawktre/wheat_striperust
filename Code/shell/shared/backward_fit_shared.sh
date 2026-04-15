353dc6a71a0a128cbfc11c28b24a2a6969d830c8
hawktre <trent.vanhawkins@gmail.com>
2026-04-15 16:35:39 -0700

    added more mem

--- Code/shell/shared/backward_fit_shared.sh
+++ Code/shell/shared/backward_fit_shared.sh
@@ -3,7 +3,7 @@
 #SBATCH -o DataProcessed/results/backward_model/logs/backward_fit_shared_%A_%a.out     # Stdout log (%A=job ID, %a=array index)
 #SBATCH -e DataProcessed/results/backward_model/logs/backward_fit_shared_%A_%a.err     # Stderr log
 #SBATCH -c 1                        # CPU cores per task
-#SBATCH --mem=4G                     # Memory per task
+#SBATCH --mem=8G                     # Memory per task
 #SBATCH --time=01:00:00             # Walltime per task
 
 set -euo pipefail
